dd96d78f23b2a4d7c604bb0778fdfcd4f650fc6f
Ayomide Johnson Salami <auomidejohnson@gmail.com>
2023-07-10 22:41:32 +0100

    added key and new fingerprint

--- .circleci/files/backend/backend.tf
+++ .circleci/files/backend/backend.tf
@@ -34,7 +34,7 @@ egress {
 resource "aws_instance" "EC2Instance" {
   instance_type = "t2.micro"
   security_groups = [aws_security_group.InstanceSecurityGroup.name]
-  key_name      = aws_key_pair.devkey.key_name
+  key_name      = "udapeople"
   ami           = "ami-068663a3c619dd892" # Replace with the appropriate AMI ID for your region and requirements
 
   tags = {
@@ -45,20 +45,20 @@ resource "aws_instance" "EC2Instance" {
 
 # Create a .pem file
 # RSA key of size 2048 bits
-resource "tls_private_key" "new-key" {
-  algorithm = "RSA"
-  rsa_bits  = 2048
-}
+# resource "tls_private_key" "new-key" {
+#   algorithm = "RSA"
+#   rsa_bits  = 2048
+# }
 
-resource "local_sensitive_file" "private_key" {
-  filename          = "backend-key.pem"
-  content = tls_private_key.new-key.private_key_pem
-  file_permission   = "0700"
-}
+# resource "local_sensitive_file" "private_key" {
+#   filename          = "backend-key.pem"
+#   content = tls_private_key.new-key.private_key_pem
+#   file_permission   = "0700"
+# }
 
 
-# Create a Key Pair
-resource "aws_key_pair" "devkey" {
-  key_name   = "backend-key"
-  public_key = tls_private_key.new-key.public_key_openssh
-}
+# # Create a Key Pair
+# resource "aws_key_pair" "devkey" {
+#   key_name   = "backend-key"
+#   public_key = tls_private_key.new-key.public_key_openssh
+# }
