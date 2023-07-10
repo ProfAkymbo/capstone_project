8485a0b710e062193d92a1eed2f70ff22b343266
Ayomide Johnson Salami <auomidejohnson@gmail.com>
2023-07-10 13:34:56 +0100

    fixing bugs in frontend

--- .circleci/files/frontend/frontend.tf
+++ .circleci/files/frontend/frontend.tf
@@ -7,12 +7,46 @@ variable "ID" {
 # Resources
 resource "aws_s3_bucket" "WebsiteBucket" {
   bucket = "udapeople-${var.ID}"
-  acl    = "public-read"
 
   website {
     index_document = "index.html"
     error_document = "404.html"
   }
+}
+
+resource "aws_s3_bucket_website_configuration" "website" {
+  bucket = aws_s3_bucket.WebsiteBucket.id
+  index_document {
+    suffix = "index.html"
+  }
+  error_document {
+    key = "404.html"
+    }
+}
+
+resource "aws_s3_bucket_ownership_controls" "website" {
+  bucket = aws_s3_bucket.WebsiteBucket.id
+  rule {
+    object_ownership = "BucketOwnerPreferred"
+  }
+}
+
+resource "aws_s3_bucket_public_access_block" "website" {
+  bucket = aws_s3_bucket.WebsiteBucket.id
+  block_public_acls       = false
+  block_public_policy     = false
+  ignore_public_acls      = false
+  restrict_public_buckets = false
+}
+
+resource "aws_s3_bucket_acl" "website" {
+  depends_on = [
+    aws_s3_bucket_ownership_controls.website,
+    aws_s3_bucket_public_access_block.website,
+  ]
+
+  bucket = aws_s3_bucket.WebsiteBucket.id
+  acl    = "public-read"
 }
 
 resource "aws_s3_bucket_policy" "WebsiteBucketPolicy" {
